1f7d979e08d88a35afd2bd653160555b66e4b813
Najaf Sharifov <nwerifov@mail.ru>
2023-03-06 00:49:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Task 2.11
+//1
+
+/*let arr = ["Liam", "Robert", "James"];
+arr[1] = "Classified";
+console.log(arr[1]);*/
+
+//2
+/*let arr = ["New Hope", "The Empire Strikes Back", "Return of the Jdi"];
+let arr_1 =["Episode 4: ", "Episode 5: ", "Episode 6: "];
+let arr_2;
+for (let i = 0; i < arr.length; i++) {
+     arr_2 = arr_1[i] + arr[i];
+     console.log(arr_2);
+}*/
+
+//3
+/*let num = prompt("Enter 5 numbers separated by commas");
+let splitted = num.split(",")
+let arr = [splitted[0],splitted[1],splitted[2],splitted[3],splitted[4]];
+for(let i = 0;i<arr.length;i++){
+   console.log(Math.min(...arr));
+}*/
+
+
